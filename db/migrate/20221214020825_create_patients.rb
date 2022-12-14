e08a5013f991b5774fac1673e433672ef2791eb2
Kevin Kelley <kkelley622@gmail.com>
2022-12-13 20:20:26 -0600

    Succesfully migrated patients table

--- db/migrate/20221214020825_create_patients.rb
+++ db/migrate/20221214020825_create_patients.rb
@@ -1,4 +1,11 @@
 class CreatePatients < ActiveRecord::Migration[6.1]
   def change
+    create_table :patients do |t|
+      t.string :name
+      t.integer :age 
+      t.string :diagnosis
+      t.integer :physician_id
+      t.integer :case_manager_id
+    end
   end
 end
